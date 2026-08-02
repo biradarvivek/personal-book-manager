8b18d60be88adfbe906cba12db1be11c5721b0bd
biradarvivek <biradarvivek03@gmail.com>
2026-08-02 18:07:18 +0530

    fix: remove unused token handling and redirect logic in middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,35 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token");
-
-  console.log("Middleware Token:", token);
-   console.log(
-    "Token:",
-    request.cookies.get("token")
-  );
-  const isAuthPage =
-    request.nextUrl.pathname === "/login" ||
-    request.nextUrl.pathname === "/register";
-
-  if (token && isAuthPage) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
-
-  if (!token && !isAuthPage) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  console.log("Cookies:", request.cookies.getAll());
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/library/:path*",
-    "/analytics/:path*",
-    "/settings/:path*",
-    "/login",
-    "/register",
-  ],
-};
